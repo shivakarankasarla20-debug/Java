62a14c214762417c18eb26422baf2e926dc9842d
shivakarankasarla20-debug <shivakarankasarla20@gmail.com>
2026-03-03 19:36:04 +0530

    integer to string

---
+++ strt.java
@@ -0,0 +1,11 @@
+class Strt
+{
+    public static void main(String args[])
+    {
+        int num = 13;
+        System.out.println(num);
+
+        String data = String.valueOf(num);
+        System.out.println(data);
+    }
+}
