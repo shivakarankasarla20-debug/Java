4aab3db16d7b6b9f7bcebef692e72f8a2053e913
shivakarankasarla20-debug <shivakarankasarla20@gmail.com>
2026-03-03 11:36:23 +0530

    double to integer

---
+++ DTI.java
@@ -0,0 +1,11 @@
+class DTI
+{
+    public static void main(String args[])
+    {
+        double num = 12.25;
+        System.out.println(num);
+
+        int data = (int) num;   // Explicit casting
+        System.out.println(data);
+    }
+}
