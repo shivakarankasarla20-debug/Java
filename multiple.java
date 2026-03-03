29bd652ef77a5ab5da026458bf97347052b80120
shivakarankasarla20-debug <shivakarankasarla20@gmail.com>
2026-03-03 11:20:21 +0530

    multiples

---
+++ multiple.java
@@ -0,0 +1,11 @@
+class multiple
+{
+    public static void main(String args[])
+    {
+        int n = 7;
+        for(int i = 1; i <= 10; i++)
+        {
+            System.out.println(n * i);
+        }
+    }
+}
