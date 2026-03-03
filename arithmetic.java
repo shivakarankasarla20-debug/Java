25d256f2afd7390e8e28ca75ede67f95d09a15a1
shivakarankasarla20-debug <shivakarankasarla20@gmail.com>
2026-03-03 11:04:47 +0530

    Arithmetic operations

---
+++ arithmetic.java
@@ -0,0 +1,11 @@
+class arithmetic
+{
+    public static void main(String args[])
+    {
+        int a=10,b=16;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+    }
+}
