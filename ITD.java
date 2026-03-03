6bf0c300f42e6f39e5cbedc6dccbb64d17082ce7
shivakarankasarla20-debug <shivakarankasarla20@gmail.com>
2026-03-03 11:32:54 +0530

    Integer to double

---
+++ ITD.java
@@ -0,0 +1,10 @@
+class ITD
+{
+    public static void main(String args[])
+    {
+        int num=12;
+        System.out.println(num);
+        double data=num;
+        System.out.println(data);
+    }
+}
