e6dc3bb919321d49880d98559b4c601f4ed6be02
kasarla shivakaran Reddy <shivakarankasarla20@gmail.com>
2026-04-02 22:56:33 +0530

    Exception

---
+++ Exception.java
@@ -0,0 +1,14 @@
+class Exception
+{
+public static void main(String args[])
+{
+try{
+int arr[]={1,2,3,4,5};
+System.out.println(arr[5]);
+}
+catch(ArrayIndexOutOfBoundsException e)
+{
+System.out.println("index out of range");
+}
+}
+}
