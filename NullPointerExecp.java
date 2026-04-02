b7d7ce645dcb8a726d1ef02afdcfe0ae701a0f90
kasarla shivakaran Reddy <shivakarankasarla20@gmail.com>
2026-04-02 22:54:03 +0530

    NullPointerExecp.java

---
+++ NullPointerExecp.java
@@ -0,0 +1,14 @@
+class NullPointerExecp
+{
+public static void main(String args[])
+{
+try{
+String s=null;
+System.out.println(s.length());
+}
+catch(NullPointerException e)
+{
+System.out.println("null value found");
+}
+}
+}
