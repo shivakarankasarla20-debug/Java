5da8f4c611c0fea8ea1c5d1670bd3bf62cde4aae
kasarla shivakaran Reddy <shivakarankasarla20@gmail.com>
2026-05-08 19:53:53 +0530

    leetcode:palindrome problem

---
+++ palindrome1.java
@@ -0,0 +1,15 @@
+class palindrome1 {
+    public boolean isPalindrome(String s) {
+
+        s = s.toLowerCase();
+        s = s.replaceAll("[^a-z0-9]", "");
+
+        String rev = "";
+
+        for(int i = s.length() - 1; i >= 0; i--) {
+            rev = rev + s.charAt(i);
+        }
+
+        return s.equals(rev);
+    }
+}
